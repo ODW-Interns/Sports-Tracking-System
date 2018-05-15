bd7f48e8851ff6b86e1c2042d0091854bda47e66
Avi-Cohen <avi@1degreeworld.com>
2018-05-15 15:08:04 -0700

    added 2 constructors to Game class, and created GamesList class

---
+++ src/main/java/IdentityClasses/GamesList.java
@@ -0,0 +1,27 @@
+package IdentityClasses;
+
+
+import java.util.ArrayList;
+
+
+public class GamesList extends ArrayList<Game> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GamesList () {}
+	
+	public void addGame(String date_, String time_, String aTeam_, int aScore_,
+			String hTeam_, int hScore_, int attendence_ ) {
+		Game addedGame = new Game(date_, time_, aTeam_, aScore_, hTeam_, hScore_, attendence_);
+		addToList(addedGame);
+	}
+	
+	public void addToList(Game game_) {
+		add(game_);
+	}
+	
+	
+}
