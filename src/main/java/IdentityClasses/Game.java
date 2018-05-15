18c60c7f0c7647cdd71b23ad91f28dc057060844
Avi-Cohen <avi@1degreeworld.com>
2018-05-15 14:55:56 -0700

    GamesList class was added , Game class constructors were added as well

--- src/main/java/IdentityClasses/Game.java
+++ src/main/java/IdentityClasses/Game.java
@@ -4,9 +4,30 @@ public class Game {
 
 	private String date;
 	private String time;
-	private String location;
+	//private String location;
 	private String hTeam, aTeam;
 	private int hTeamScore, aTeamScore, attendence;
+	
+	
+	
+	
+	public Game() {
+		this("general","general","general", 0, "general",0,0);
+	}
+	
+	
+	
+	public Game(String date, String time, String aTeam, int aTeamScore, String hTeam, int hTeamScore,
+			int attendence) {
+
+		setDate(date);
+		setTime(time);
+		sethTeam(hTeam);
+		setaTeam(aTeam);
+		setaTeamScore(aTeamScore);
+		sethTeamScore(hTeamScore);
+		setAttendence(attendence);
+	}
 	
 	public String getDate() {
 		return date;
@@ -20,12 +41,14 @@ public class Game {
 	public void setTime(String tim) {
 		time = tim;
 	}
+	/*
 	public String getLocation() {
 		return location;
 	}
 	public void setLocation(String loc) {
 		location = loc;
 	}
+	*/
 	public String gethTeam() {
 		return hTeam;
 	}
