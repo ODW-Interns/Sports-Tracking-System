99274d6dbffa7bac92b9e46f3df93f93b7131e08
Avi-Cohen <avi@1degreeworld.com>
2018-05-15 11:58:06 -0700

    Add the Game class

---
+++ Game.java
@@ -0,0 +1,62 @@
+
+public class Game {
+
+	private String date;
+	private String time;
+	private String location;
+	private String hTeam, aTeam;
+	private int hTeamScore, aTeamScore, attendence;
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String dat) {
+		date = dat;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String tim) {
+		time = tim;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String loc) {
+		location = loc;
+	}
+	public String gethTeam() {
+		return hTeam;
+	}
+	public void sethTeam(String hT) {
+		hTeam = hT;
+	}
+	public String getaTeam() {
+		return aTeam;
+	}
+	public void setaTeam(String aT) {
+		aTeam = aT;
+	}
+	public int gethTeamScore() {
+		return hTeamScore;
+	}
+	public void sethTeamScore(int hTScore) {
+		hTeamScore = hTScore;
+	}
+	public int getaTeamScore() {
+		return aTeamScore;
+	}
+	public void setaTeamScore(int aTScore) {
+		aTeamScore = aTScore;
+	}
+	public int getAttendence() {
+		return attendence;
+	}
+	public void setAttendence(int att) {
+		attendence = att;
+	}
+	
+	
+	
+	
+	}
