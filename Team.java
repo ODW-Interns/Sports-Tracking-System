eff15af2b0a7d0c6e21bd9522d74e4c5e37545ef
tuanaut <tuan@1degreeworld.com>
2018-05-15 11:38:07 -0700

    Added Team identity class

---
+++ Team.java
@@ -0,0 +1,46 @@
+
+public class Team {
+
+	private String teamName; 
+	private String location;
+	private int numPlayers;
+	
+	public Team() {
+		// Call Constructor
+		this("general", "general", 0);
+	}
+	
+	public Team(String team, String loc, int players){
+		teamName = team;
+		location = loc;
+		numPlayers = players;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String team) {
+		teamName = team;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String loc) {
+		location = loc;
+	}
+
+	public int getNum_players() {
+		return numPlayers;
+	}
+
+	public void setNumPlayers(int players) {
+		numPlayers = players;
+	}
+	
+	
+	
+
+}
