5a27913e0857c60215115d95d73e323a44741630
Avi-Cohen <avi@1degreeworld.com>
2018-05-16 14:55:34 -0700

    Reading from the teams list txt file and set the TeamList

--- src/main/java/IOClasses/TeamListFileReader.java
+++ src/main/java/IOClasses/TeamListFileReader.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.StringTokenizer;
 
-import IdentityClasses.Game;
 import IdentityClasses.Team;
 
 
@@ -38,30 +37,31 @@ public class TeamListFileReader {
 		try (BufferedReader reader = new BufferedReader(is_)) {
 			StringTokenizer tokenizer;
 			String line;
-			Team team;
+			
 			while ((line = reader.readLine()) != null) {
 				// don't process empty lines
 				if ("".equals(line))
 					continue;
 
-				team = new Team();
+				Team team = new Team();
 				tokenizer = new StringTokenizer(line, DELIM);
 
 				try {
 					team.setLocation(tokenizer.nextToken());
 				} catch (Exception e_) {
-					System.err.println("setDate:" + e_.toString());
+					System.err.println("setLocation:" + e_.toString());
 					continue;
 				}
 
 				try {
 					team.setTeamName(tokenizer.nextToken());
 				} catch (Exception e_) {
-					System.err.println("setTime:" + e_.toString());
+					System.err.println("setTeamName:" + e_.toString());
 					continue;
 				}
-			}
 				listofTeams_.add(team);
+			}
+				
 		}catch (Exception e_) {
 			e_.printStackTrace();
 		}
