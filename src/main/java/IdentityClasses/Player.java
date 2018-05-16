d8a7a1e975a3cd099c35ba7a54d63c4e30dc8cf0
tuanaut <tuan@1degreeworld.com>
2018-05-16 14:23:08 -0700

    Added Identity class 'Player'

---
+++ src/main/java/IdentityClasses/Player.java
@@ -0,0 +1,54 @@
+package IdentityClasses;
+
+public class Player {
+
+	String firstName;
+	String lastName;
+	int jerseyNum;
+	String position;
+	
+	public Player() {
+		this("N/A", "N/A", "N/A", -1);
+	}
+	
+	public Player(String first_, String last_, String pos_, int jersey_){
+		firstName = first_;
+		lastName = last_;
+		position = pos_;
+		jerseyNum = jersey_;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String first_) {
+		firstName = first_;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String last_) {
+		lastName = last_;
+	}
+
+	public int getJerseyNum() {
+		return jerseyNum;
+	}
+
+	public void setJerseyNum(int jersey_) {
+		jerseyNum = jersey_;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String pos_) {
+		position = pos_;
+	}
+	
+	
+}
