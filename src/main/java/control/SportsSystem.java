defa4217bbc9478fec67c14415ce68d781f3ff71
tuanaut <tuan@1degreeworld.com>
2018-05-22 14:38:29 -0700

    Added input file for upcoming NFL Games

--- src/main/java/control/SportsSystem.java
+++ src/main/java/control/SportsSystem.java
@@ -19,14 +19,41 @@ public class SportsSystem {
 		int sportChosen = -1;	
 		int request = -1;
 		int numOfInvalidUpcomingGames = 0;
+		
+		GamesList genericUpcomingGamesList = new GamesList();
+		GamesList genericPastGamesList = new GamesList();
+		TeamsList genericTeamsList = new TeamsList();
 		// List of Games from the past NBA season
-		GamesList listofPastGames = StoreDataFromInputFile.storeDataIntoPastGameList("/17-18NBA_RegSzn.txt");
+		GamesList listofPastNBAGames = StoreDataFromInputFile.storeDataIntoPastGameList("/17-18NBA_RegSzn.txt");
 		// List of upcoming Games in the NBA
-		GamesList listofUpcomingGames = StoreDataFromInputFile.storeDataIntoUpcomingGameList("/NBA_Upcoming.txt");
+		GamesList listofUpcomingNBAGames = StoreDataFromInputFile.storeDataIntoUpcomingGameList("/NBA_Upcoming.txt");
 		// List of Players in NBA teams 
+		// List of Teams in the NBA
 		TeamsList listofNBATeams = StoreDataFromInputFile.storeDataIntoTeamsList("/NBA_Teams.txt");
 		StoreDataFromInputFile.storeDataIntoPlayersList("/AllCurrentNBARosters.txt", listofNBATeams);
-				
+		
+		// List of Games from the past NFL season
+		GamesList listofPastNFLGames = StoreDataFromInputFile.storeDataIntoPastGameList("/17-18NFL_RegSzn.txt");
+		// List of upcoming Games in the NFL Games
+		// List of Players in NFL teams
+		// List of Teams in the NFL
+		TeamsList listofNFLTeams = StoreDataFromInputFile.storeDataIntoTeamsList("/NFL_Teams.txt");
+		
+		// List of Games from the past MLB season
+		GamesList listofPastMLBGames = StoreDataFromInputFile.storeDataIntoPastGameList("/17-18MLB_RegSzn.txt");
+		//List of upcoming games in the MLB
+		//List of players in the MLB
+		//List of teams in the MLB
+		TeamsList listofMLBTeams = StoreDataFromInputFile.storeDataIntoTeamsList("/MLB_Teams.txt");
+		
+		//List of Games from the past NHL season
+		GamesList listofPastNHLGames = StoreDataFromInputFile.storeDataIntoPastGameList("/17-18NHL_RegSzn.txt");
+		// List of upcoming games in the NHL
+		// List of players in the MLB
+		//List of teams in the NHL
+		TeamsList listofNHLTeams = StoreDataFromInputFile.storeDataIntoTeamsList("/NHL_Teams.txt");
+
+
 		System.out.println("<<<<<<<<<<Welcome to the Sports Tracking System>>>>>>>>>>");
 		
 		/*
@@ -43,13 +70,34 @@ public class SportsSystem {
 			PrintToLog.printMainMenuAlert();
 			sportChosen = ControllerToHandleUserInput.readSportsChosenByUser(); // read in valid input from user
 			PrintToLog.printSportCategory(sportChosen);
+			
+			if(sportChosen == 1) {
+				genericUpcomingGamesList = listofUpcomingNBAGames;
+				genericPastGamesList = listofPastNBAGames;
+				genericTeamsList = listofNBATeams;
+			}
+			else if(sportChosen == 2) {
+				//genericUpcomingGamesList = listofUpcomingNFLGames;
+				genericPastGamesList = listofPastNFLGames;
+				genericTeamsList = listofNFLTeams;
+			}
+			else if(sportChosen == 3) {
+				//genericUpcomingGamesList = listofUpcomingMLBGames;
+				genericPastGamesList = listofPastMLBGames;
+				genericTeamsList = listofMLBTeams;
+			}
+			else if(sportChosen == 4) {
+				//genericUpcomingGamesList = listofUpcomingNHLGames;
+				genericPastGamesList = listofPastNHLGames;
+				genericTeamsList = listofNHLTeams;
+			}
 			if(ControllerToHandleUserInput.sportChosenIsValid(sportChosen) && sportChosen != 0){
 				do { 
 					// Inside individual sport category
 					// Refresh
-					numOfInvalidUpcomingGames = RealTimeDataChecker.thereAreInvalidUpcomingGames(listofUpcomingGames);
+					numOfInvalidUpcomingGames = RealTimeDataChecker.thereAreInvalidUpcomingGames(listofUpcomingNBAGames);
 					if(numOfInvalidUpcomingGames > 0) {
-					RealTimeDataChecker.refreshDataLists(numOfInvalidUpcomingGames, listofUpcomingGames, listofPastGames);
+					RealTimeDataChecker.refreshDataLists(numOfInvalidUpcomingGames, listofUpcomingNBAGames, listofPastNBAGames);
 					}
 					request = ControllerToHandleUserInput.readRequestByUser();
 					switch(request) {
@@ -57,19 +105,19 @@ public class SportsSystem {
 					case 1: //1. Refresh Both upcoming games and finished games list
 							//2.Print to Log the list of finished games
 							System.out.println("1 was selected");
-							numOfInvalidUpcomingGames = RealTimeDataChecker.thereAreInvalidUpcomingGames(listofUpcomingGames);
+							numOfInvalidUpcomingGames = RealTimeDataChecker.thereAreInvalidUpcomingGames(listofUpcomingNBAGames);
 							if(numOfInvalidUpcomingGames > 0) {
-								RealTimeDataChecker.refreshDataLists(numOfInvalidUpcomingGames, listofUpcomingGames, listofPastGames);
+								RealTimeDataChecker.refreshDataLists(numOfInvalidUpcomingGames, listofUpcomingNBAGames, listofPastNBAGames);
 							}
-							PrintToLog.logGamesList(listofPastGames);
+							PrintToLog.logGamesList(listofPastNBAGames);
 							break;
 					case 2: // 1. Refresh both upcoming games and finished gams list
 							// 2. Print to Log the list of upcoming games
-							numOfInvalidUpcomingGames = RealTimeDataChecker.thereAreInvalidUpcomingGames(listofUpcomingGames);
+							numOfInvalidUpcomingGames = RealTimeDataChecker.thereAreInvalidUpcomingGames(listofUpcomingNBAGames);
 							if(numOfInvalidUpcomingGames > 0) {
-								RealTimeDataChecker.refreshDataLists(numOfInvalidUpcomingGames, listofUpcomingGames, listofPastGames);
+								RealTimeDataChecker.refreshDataLists(numOfInvalidUpcomingGames, listofUpcomingNBAGames, listofPastNBAGames);
 							}
-							PrintToLog.logGamesList(listofUpcomingGames);
+							PrintToLog.logGamesList(listofUpcomingNBAGames);
 							break;
 					case 3: //Print out all teams & let user choose from team to see current roster
 							PrintToLog.logAllRosters(listofNBATeams);
