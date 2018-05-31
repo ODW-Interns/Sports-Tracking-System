4b53d91b7a0327a1ea314f354c2dcb4e66832e12
Avi-Cohen <avi@1degreeworld.com>
2018-05-31 10:51:14 -0700

    fixed playerslist class (HashMap)

--- src/main/java/com/sts/concreteModel/PlayersList.java
+++ src/main/java/com/sts/concreteModel/PlayersList.java
@@ -3,18 +3,23 @@ package com.sts.concreteModel;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.sts.abstractModel.Player;
+import com.sts.abstractModel.Team;
 
 public class PlayersList {
 	
-	private ConcurrentHashMap<Integer, Player> _playersMap;
-	
-	public void addPlayer(String first_, String last_, int jersey_) {
+	private ConcurrentHashMap<Integer, Player> PlayerMap_;
+
+	public void addPlayer(int PlayerID_, int jerseyNum_, String firstName_, String lastName_) {
 		
 		//add(new Player(first_, last_, jersey_));
 	}
 	
+	public PlayersList() {
+		PlayerMap_ = new ConcurrentHashMap<Integer, Player>();
+	}
+	
 	public ConcurrentHashMap<Integer, Player> returnPlayersMap(){
-		return _playersMap;
+		return PlayerMap_;
 	}
 
 	
