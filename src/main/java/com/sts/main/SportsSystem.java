7ea094930f98966a0d29a8d0c4fd10d047477188
Akhil David <you@example.com>
2018-06-12 08:50:32 -0700

    MenuFixed

--- src/main/java/com/sts/main/SportsSystem.java
+++ src/main/java/com/sts/main/SportsSystem.java
@@ -2,6 +2,7 @@ package com.sts.main;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import org.slf4j.Logger;
@@ -38,78 +39,77 @@ public class SportsSystem {
 	    Scanner input= new Scanner(System.in);
 	    _logger.info("Welcome to sports Tracking system");
 
-	    boolean isNumber;
+	    boolean isNumber = false;
 	    int choice = 0;   
 	    String fileName;
 	    boolean c = true;
 	    
-	    while (c) {
-	    	
-	    	_logger.info("1: Enter 1 to display list of finished games");
-	    	_logger.info("2: Enter 2 to display list of upcoming games");
-	    	_logger.info("3: Enter 3 to update player(s))");
-	    	_logger.info("4: Enter 4 to exit");
-		    
-		    do {
-		    	
-		    	if(input.hasNextInt() ){
-			    	
-			   		choice=input.nextInt();
-			   		isNumber=true;	
-			   		
-			   	}
-			    else {
-			    	
-			    	_logger.info("Please choose an integer value from the above options");
-			    	isNumber=false;
-			    	input.nextInt();
-			    	
-			    }
-		    	
-		    }while(!isNumber);
-			 
-		    
-		    switch (choice) {
-			case 1:
+	    
+	    
+	    
+			do {
 				
-				listofGames.logFinishedGames(listofPlayers);
-				
-				break;
-			case 2:
-				
-			    listofGames.logUpcomingGames(listofPlayers);
-				
-			    break;
-			case 3:
-				input.nextLine();
-				_logger.info("Enter the file name to update player(s) you want");
-				fileName = input.nextLine();
-				String path = "/";
-				path = path + fileName;
-				try {
-					
-					System.out.println(path);
-					StoreDataFromInputFile.storeDataIntoPlayerList(path, listofGames, listofTeams, listofPlayers);
-				}
-				catch(Exception e_1) {
-					_logger.error("File not found: " + e_1.toString());
-				}
-				break;
-			case 4:
-				System.exit(0);
-				break;
+					while (c) {
 
-			default:
+						_logger.info("1: Enter 1 to display list of finished games");
+						_logger.info("2: Enter 2 to display list of upcoming games");
+						_logger.info("3: Enter 3 to update player(s))");
+						_logger.info("4: Enter 4 to exit");
+
+						if (input.hasNextInt()) {
+
+							choice = input.nextInt();
+							isNumber = true;
+
+						} else {
+
+							_logger.info("Please choose an integer value from the above options");
+							isNumber = false;
+							input.next();
+
+						}
+
+						switch (choice) {
+						case 1:
+
+							listofGames.logFinishedGames(listofPlayers);
+
+							break;
+						case 2:
+
+							listofGames.logUpcomingGames(listofPlayers);
+
+							break;
+						case 3:
+							input.nextLine();
+							_logger.info("Enter the file name to update player(s) you want");
+							fileName = input.nextLine();
+							String path = "/";
+							path = path + fileName;
+							try {
+
+								System.out.println(path);
+								StoreDataFromInputFile.storeDataIntoPlayerList(path, listofGames, listofTeams,
+										listofPlayers);
+							} catch (Exception e_1) {
+								_logger.error("File not found: " + e_1.toString());
+							}
+							break;
+						case 4:
+							System.exit(0);
+							break;
+
+						default:
+
+							_logger.info("Wrong choice input. Please choose from the following options");
+
+							break;
+
+						}
+					}
 				
-				_logger.info("Wrong choice input. Please choose from the following options");
-				
-				break;
-			
-				
-			
-			}
-		}
-		
+			} while (!isNumber);
+		input.close();
 
 	}
 	
