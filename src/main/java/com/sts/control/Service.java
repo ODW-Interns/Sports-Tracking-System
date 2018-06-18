b4f3001aad33f1ada89069e6ffcbbc4054707f5f
Akhil David <you@example.com>
2018-06-18 12:07:15 -0700

    Structure for Service Class Updated

--- src/main/java/com/sts/control/Service.java
+++ src/main/java/com/sts/control/Service.java
@@ -1,5 +1,21 @@
 package com.sts.control;
 
 public class Service {
-
+	
+		public void createGames() {
+			
+		}
+		
+		public void createPlayers() {
+			
+			
+		}
+		public void createTeam() {
+			
+			
+		}
+		public void movePlayer() {
+			
+			
+		}
 }
